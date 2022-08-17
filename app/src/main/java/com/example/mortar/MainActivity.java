70a9dbaeb396fe8ce26fb343e23503327f283a28
vebat <genaxex51@gmail.com>
2022-08-17 18:51:11 +0300

    Work example of work with fire tables

--- app/src/main/java/com/example/mortar/MainActivity.java
+++ app/src/main/java/com/example/mortar/MainActivity.java
@@ -57,20 +57,20 @@ public class MainActivity extends AppCompatActivity {
         String in= stringn.getText().toString();
         int i=Integer.parseInt(in);
         // Создание переменных и списка для получения дальности
-        int dif;
+        double difratio;
         int listn = 0;
-        int distdif1;
-        int distdif2;
-        int scopedivdif1;
-        int scopedivdif2;
-        int scopethdif1;
-        int scopethdif2;
-        int timedif1;
-        int timedif2;
-        int ratioupdif1;
-        int ratioupdif2;
-        int ratiodowndif1;
-        int ratiodowndif2;
+        double distdif1=0;
+        double distdif2=0;
+        double scopedivdif1=0;
+        double scopedivdif2=0;
+        double scopethdif1=0;
+        double scopethdif2=0;
+        double timedif1=0;
+        double timedif2=0;
+        double ratioupdif1=0;
+        double ratioupdif2=0;
+        double ratiodowndif1=0;
+        double ratiodowndif2=0;
         String dist = "";
         String scoped = "";
         String scopet = "";
@@ -103,27 +103,64 @@ public class MainActivity extends AppCompatActivity {
                     cursor.moveToNext();
                 }
             }
+
             // Получение данных из первой строки
-            distdif1 = Integer.parseInt(cursor.getString(1));
-            scopedivdif1 = Integer.parseInt(cursor.getString(2));
-            scopethdif1 = Integer.parseInt(cursor.getString(3));
-            timedif1 = Integer.parseInt(cursor.getString(4));
-            ratioupdif1 = Integer.parseInt(cursor.getString(5));
-            ratiodowndif1 = Integer.parseInt(cursor.getString(6));
+            dist = cursor.getString(1);
+            scoped = cursor.getString(2);
+            scopet = cursor.getString(3);
+            t = cursor.getString(4);
+            ratiou = cursor.getString(5);
+            ratiod = cursor.getString(6);
+
+            try {
+                distdif1 = Double.parseDouble(dist);
+                scopedivdif1 = Double.parseDouble(scoped);
+                scopethdif1 = Double.parseDouble(scopet);
+                timedif1 = Double.parseDouble(t);
+                ratioupdif1 = Double.parseDouble(ratiou);
+                if(ratiod!=null){
+                    ratiodowndif1 = Double.parseDouble(ratiod);
+                } else{
+                    ratiodowndif1=0;
+                }
+            } catch(NumberFormatException e){
+                distance.setText("Ошибка при вводе данных");
+            }
 
             cursor.moveToNext();
 
             // получение данных из второй строки
-            distdif2 = Integer.parseInt(cursor.getString(1));
-            scopedivdif2 = Integer.parseInt(cursor.getString(2));
-            scopethdif2 = Integer.parseInt(cursor.getString(3));
-            timedif2 = Integer.parseInt(cursor.getString(4));
-            ratioupdif2 = Integer.parseInt(cursor.getString(5));
-            ratiodowndif2 = Integer.parseInt(cursor.getString(6));
-            
+            try {
+                distdif2 = Double.parseDouble(cursor.getString(1));
+                scopedivdif2 = Double.parseDouble(cursor.getString(2));
+                scopethdif2 = Double.parseDouble(cursor.getString(3));
+                timedif2 = Double.parseDouble(cursor.getString(4));
+                ratioupdif2 = Double.parseDouble(cursor.getString(5));
+                if(ratiod!=null){
+                    ratiodowndif2 = Double.parseDouble(ratiod);
+                } else{
+                    ratiodowndif2=0;
+                }
+            } catch (NumberFormatException e) {
+                distance.setText("Ошибка при вводе данных");
+            }
 
 
-        }
+            distdif2=distdif2-distdif1;
+            scopedivdif2=scopedivdif2-scopedivdif1;
+            scopethdif2=scopethdif2-scopethdif1;
+            timedif2=timedif2-timedif1;
+            ratioupdif2=ratioupdif2-ratioupdif1;
+            ratiodowndif2=ratiodowndif2-ratiodowndif1;
+
+            difratio=(i-distdif1)/distdif2;
+            dist=String.valueOf(i);
+            scoped=String.valueOf(scopedivdif2*difratio+scopedivdif1);
+            scopet=String.valueOf(scopethdif2*difratio+scopethdif1);
+            t=String.valueOf(timedif2*difratio+timedif1);
+            ratiou=String.valueOf(ratioupdif2*difratio+ratioupdif1);
+            ratiod=String.valueOf(ratiodowndif2*difratio+ratiodowndif1);
+     }
 
         cursor.close();
         // Передача данных в интерфейс
